79b8c500245dedf662963812d204f0621a529166
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-05-12 21:34:09 +0100

    modified:   src/Upvote.js

--- src/Upvote.js
+++ src/Upvote.js
@@ -3,16 +3,18 @@ import { useState } from "react";
 
 const Upvote = ({ existingScore, id }) => {
   const [upVote, setUpVote] = useState(existingScore);
+  const [addVote, setAddVote] = useState(true);
 
   const patchUrl = `http://localhost:7000/comments/${id}`;
 
   const downVote = () => {
-    if (upVote > 0) {
+    if (addVote && upVote > 0) {
       setUpVote(upVote - 1);
     }
   };
 
   useEffect(() => {
+    setAddVote(false);
     const newVote = {
       score: upVote,
     };
@@ -21,12 +23,13 @@ const Upvote = ({ existingScore, id }) => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newVote),
     });
+    setAddVote(true);
   }, [upVote]);
 
   return (
     <section className="upvote">
       <div className="upvote-button">
-        <button onClick={() => setUpVote(upVote + 1)}>
+        <button onClick={() => addVote && setUpVote(upVote + 1)}>
           <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
