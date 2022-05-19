115e548f381688432929be38e63082a142a931e4
Osayi Akoko <osayistreams@gmail.com>
2022-05-19 04:01:38 +0100

    reply form component

---
+++ src/ReplyForm.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const ReplyForm = ({ currentUser, commentId, commentAuthor, addReply }) => {
+  const [content, setContent] = useState("");
+
+  // useEffect(() => {
+  //   setContent(comment ? comment.content : "");
+  // }, [comment]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await addReply(commentId, {
+      id: new Date().getTime(),
+      content,
+      createdAt: new Date().toISOString(),
+      score: 0,
+      user: currentUser,
+      replyingTo: commentAuthor,
+    });
+    setContent("");
+  };
+
+  return (
+    <div>
+      <form className="form card" onSubmit={handleSubmit}>
+        <img src={currentUser.image.png} alt="avi" className="profile-img" />
+        <textarea
+          type="text"
+          className="input"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <button className="btn" type="submit">
+          SEND
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ReplyForm;
