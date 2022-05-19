c23e5ec4f6f42c71fcf693c82e7bef2d7c122cb2
Osayi Akoko <osayistreams@gmail.com>
2022-05-19 04:02:18 +0100

    format created at

--- src/Identity.js
+++ src/Identity.js
@@ -1,6 +1,20 @@
-import React from "react";
+import dayjs from 'dayjs'
+import relativeTime from "dayjs/plugin/relativeTime";
+
 
 const Identity = ({ currentUser, user, createdAt, edited }) => {
+  // initializations
+  dayjs.extend(relativeTime)
+
+  // methods & functions
+  const getCreatedAt = () => {
+    if (createdAt.includes('ago') || createdAt.includes('Now')) {
+      return createdAt
+    }
+    return dayjs(new Date(createdAt)).fromNow()
+  }
+
+  // render
   return (
     <section className="identity">
       <img src={user.image.png} alt="avi" className="profile-img" />
@@ -8,7 +22,7 @@ const Identity = ({ currentUser, user, createdAt, edited }) => {
       {user.username === currentUser.username && (
         <h6 className="indicator">YOU</h6>
       )}
-      <p>{createdAt}</p>
+      <p>{getCreatedAt()}</p>
       {edited && <p className="edited">{edited}</p>}
     </section>
   );
